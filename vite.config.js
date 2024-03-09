e7d8388607ceca95d46c84a462edaf344d020e72
vinay <vinih0505@gmail.com>
2024-03-09 18:28:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,12 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
+export default defineConfig({
   plugins: [react()],
-    // vite config
-    define: {
-      __APP_ENV__: JSON.stringify(env.APP_ENV),
-    },
-  }
+server: {
+host: true,
+strictPort: true,
+port: 8000,
+},
 })
-
