eb027a7c059d1e6622cffbbe18f9c4c51809d87b
ViNi <vinih0505@example.com>
2024-03-10 21:33:30 +0530

    Design updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <footer className="page-footer font-small blue pt-4" style={{background:"linear-gradient(93deg, rgba(58,162,180,0.6797312675070029) 0%, rgba(253,29,227,0.38001137955182074) 48%, rgba(252,176,69,0.2399553571428571) 100%)"}}>
+    <div className="container-fluid text-center text-md-left">
+        <div className="row">
+            <div className="col-md-6 mt-md-0 mt-3">
+                <h6>Profile:</h6>
+                <h5 className="text-uppercase">VINAYAK HONNATTI</h5>
+            </div>
+        </div>
+    </div>
+</footer>
   )
 }
 
